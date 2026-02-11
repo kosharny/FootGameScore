67b3f6b14c0152f573d1131a661f37f4fbc7154b
Maksim Kosharny <maksimkosharnyjj@gmail.com>
2026-02-11 12:49:40 +0200

    Initial Commit

---
+++ FootGameScore/FootGameScoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  FootGameScoreApp.swift
+//  FootGameScore
+//
+//  Created by Maksim Kosharny on 11.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct FootGameScoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
